62957aa5dfad7be7bf322b51b2b5243630be306d
LJHO98 <ehdgoanf714@naver.com>
2024-06-12 00:27:26 +0900

    123

--- 0611_hw/static/javascript/0611_hw.js
+++ 0611_hw/static/javascript/0611_hw.js
@@ -1,11 +1,25 @@
 window.onload=function(){
-
+    document.getElementById("print").addEventListener('click', function(){
+        outPut();
+    });
 }
 
-function 랜덤(){
+function randomOp(){
+    var save = "";
 
+    for(var i=1; i <= 5 ; i++){
+        for(var j=1; j <= 4; j++){
+            save = save + String(Math.floor(Math.random() * 50 + 1));
+            if(j < 4){
+                save = save + " ";
+            }
+        }
+        save = save + "<br>";
+    }
+    return save;
 }
 
-function 나열(){
-    
+ function outPut(){
+    var number = randomOp();
+    document.getElementById("result").innerHTML = number;
 }
