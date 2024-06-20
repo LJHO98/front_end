34002a1876ead1201345afe87296a4d0acc44712
LJHO98 <ehdgoanf714@naver.com>
2024-06-20 17:38:24 +0900

    123

---
+++ card_pair/static/javascript/card_pair.js
@@ -0,0 +1,14 @@
+$(function(){
+    $("#start").click(start);
+});
+
+function start(){
+    $(".card").click(userSelect);
+}
+
+function userSelect(){
+    var idx = $(".card").index($(this));
+    console.log(idx);
+
+    $(".card img").css('display', 'block');
+}
